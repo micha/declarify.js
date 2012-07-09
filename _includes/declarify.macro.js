111377f00014d79ccb29c86e64d66223c61b0ad6
Micha Niskin <asdf@ubergibson.com>
2012-07-09 00:22:15 -0400

    cleanup

--- _includes/declarify.macro.js
+++ _includes/declarify.macro.js
@@ -141,17 +141,15 @@
   });
 
   $UI.m.macro.fn("depends", function mconj(env, meta, sexp) {
-    var sym   = gensym(),
-        attr  = into({}, keep(map(function(x) {
-          return x[0] === "ref"
-            ? [ "data-dep::"+x[1], sym ]
-            : (x[0].substr(0,1) === ":"
-                ? [ "data-"+sym+"::"+x[0].substr(1), x[1] ]
-                : null);
-        }, outof(meta))));
-
-    sexp.attr = dup(sexp.attr, attr);
-    return sexp;
+    var sym = gensym(), attr;
+    attr = into({}, keep(map(function(x) {
+      return x[0] === "ref"
+        ? [ "data-dep::"+x[1], sym ]
+        : (x[0].substr(0,1) === ":"
+            ? [ "data-"+sym+"::"+x[0].substr(1), x[1] ]
+            : null);
+    }, outof(meta))));
+    return assoc(dup(sexp), "attr", dup(sexp.attr, attr));
   });
 
 })();
