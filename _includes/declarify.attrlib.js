0539b0feb5f5b15151e0147a5d96f3adc376eb5f
Micha Niskin <asdf@ubergibson.com>
2012-07-16 12:19:13 -0400

    added class dependency module to set/unset classes

--- _includes/declarify.attrlib.js
+++ _includes/declarify.attrlib.js
@@ -1,9 +1,12 @@
 
 (function() {
   
-  $UI.m.dep("text", function attrlib_text(elem, val, same) {
-    if (val !== same)
-      elem.text(val);
+  $UI.m.dep("text", function attrlib_text(elem, val) {
+    elem.text(val);
+  });
+
+  $UI.m.dep("class", function attrlib_class(elem, val, classname) {
+    elem[(val ? "add" : "remove") + "Class"](classname);
   });
 
 })();
